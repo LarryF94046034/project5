f1130efb6dfb621b236628926730631e0e93831e
LarryChen <larry110864gmail>
2025-03-31 09:31:19 +0800

    移動蛇
    移動蛇，位置/物件更新邏輯，畫布更新邏輯

--- app.js
+++ app.js
@@ -13,17 +13,45 @@ snake[1] = { x: 60, y: 0 };
 snake[2] = { x: 40, y: 0 };
 snake[3] = { x: 20, y: 0 };
 
-for (var i = 0; i < snake.length; i++) {
-  //console.log(array[i]);
-  if (i == 0) {
-    ctx.fillStyle = "lightgreen";
-  } else {
-    ctx.fillStyle = "lightblue";
+let direction = "Right";
+
+function draw() {
+  //初始更新全黑
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (var i = 0; i < snake.length; i++) {
+    //console.log(array[i]);
+    if (i == 0) {
+      ctx.fillStyle = "lightgreen";
+    } else {
+      ctx.fillStyle = "lightblue";
+    }
+    ctx.strokeStyle = "white";
+    //x y width height
+    ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+    ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
   }
-  ctx.strokeStyle = "white";
-  //x y width height
-  ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
-  ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "Right") {
+    snakeX += unit;
+  } else if (direction == "Left") {
+    snakeX -= unit;
+  } else if (direction == "Up") {
+    snakeY += unit;
+  } else if (direction == "Down") {
+    snakeY -= unit;
+  }
+
+  let newHead = { x: snakeX, y: snakeY };
+
+  snake.pop();
+  snake.unshift(newHead);
 }
+
+let myGame = setInterval(draw, 100);
 
 console.log(snake);
